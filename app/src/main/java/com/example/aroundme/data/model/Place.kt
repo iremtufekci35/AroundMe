662d65391310b86adfa2580912f2e63fb03a1eea
Irem tufekci <113184262+iremtufekci35@users.noreply.github.com>
2025-08-19 17:48:42 +0300

    Added card view for detailed information about locations

--- app/src/main/java/com/example/aroundme/data/model/Place.kt
+++ app/src/main/java/com/example/aroundme/data/model/Place.kt
@@ -1,7 +1,21 @@
 package com.example.aroundme.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Place(
-    val name: String,
-    val lat: Double,
-    val lon: Double
+    @SerializedName("name") val name: String,
+    @SerializedName("lat") val lat: Double,
+    @SerializedName("lon") val lon: Double,
+    @SerializedName("addr:street") val street: String? = null,
+    @SerializedName("historic") val historic: String? = null,
+    @SerializedName("bus") val bus: String? = null,
+    @SerializedName("shop") val shop: String? = null,
+    @SerializedName("amenity") val amenity: String? = null,
+    @SerializedName("phone") val phone: String? = null,
+    @SerializedName("tram") val tram: Boolean? = null,
+    @SerializedName("railway") val railway: String? = null,
+    @SerializedName("public_transport") val publicTransport: String? = null,
+    @SerializedName("ferry") val ferry: String? = null,
+    @SerializedName("opening_hours") val openingHours: String? = null
 )
+
